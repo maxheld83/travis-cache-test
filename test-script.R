36ab1ba0e0ff68cd172486424c0797119357af23
Maximilian Held <info@maxheld.de>
2017-02-01 19:59:17 +0100

    now let's add a new package and see what happens
    notice that this commit includes a changed `packrat.lock` as well as
    the necessary `/src/` stuff

--- test-script.R
+++ test-script.R
@@ -1,3 +1,9 @@
 # this is just some test script
 
 1+1
+
+# now let's add some new package and use that, see whether packrat on travis detects the change and invalidates the cache
+# install.packages("babynames")
+library(babynames)
+babynames::babynames$name[1:3]
+packrat::status()
