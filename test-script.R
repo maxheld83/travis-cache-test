30082568a6a74b6e1216d1d41fcdfd7cdcf52189
Maximilian Held <info@maxheld.de>
2017-02-07 14:07:48 +0100

    replace packrat with simple setup, test remotes field

--- test-script.R
+++ test-script.R
@@ -3,14 +3,4 @@
 1+1
 
 # now let's add some new package and use that, see whether packrat on travis detects the change and invalidates the cache
-# install.packages("babynames")
-library(babynames)
-babynames::babynames$name[1:3]
-# packrat::status()
-# packrat::snapshot()
-
-# install.packages("nycflights13")
-nycflights13::airlines
-
-# install.packages("ggplot2")
-ggplot2::mpg$manufacturer[1:3]
+library(qmethod)
